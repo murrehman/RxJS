400f63e3d28f43a4bbe07864025d1d3897d3f13c
murrehman <muteeurrehmanbajwa@gmail.com>
2022-04-03 02:54:55 +0200

    Init angular and ngxbootstrap

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'frontend';
+
+  form = new FormGroup({
+    myControl: new FormControl(new Date())
+  });
+}
